135f3bf3ee08594caa85d85069155e2ad013aa20
Taylor <wwwaaavvveeerrr@gmail.com>
2019-10-15 23:12:00 -0400

    user table and api built out

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # welcome page for my group
   root 'welcome#index'
+
+  namespace :api do
+    namespace :v1 do
+
+      resources :users
+    end
+  end
 end
